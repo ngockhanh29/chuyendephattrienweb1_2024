a7b4d90c23968ccc7a52ae9f8a44d9c590079cad
ngockhanh29 <kk0434860@gmail.com>
2024-10-12 10:35:12 +0700

    1354

--- front/1354/1354.php
+++ front/1354/1354.php
@@ -22,7 +22,7 @@ $less->compileFile('less/1354.less', 'css/1354.css');
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
       
-        <link href="<?php echo $url_path ?>/css/bootstrap.min.css" rel="stylesheet" type="text/css"/
+        <link href="<?php echo $url_path ?>/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
               <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
         <link href="<?php echo $url_path ?>/css/1354.css" rel="stylesheet" type="text/css"/>
  
